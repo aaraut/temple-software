bcef3ca44ad7b2df9890f61247ad81002db1baf6
aaraut <alekh.raut@outlook.com>
2025-12-05 14:34:05 +0530

    chore: add initial Spring Boot (Java 21) backend project

---
+++ backend/src/main/java/in/temple/backend/TempleBackendApplication.java
@@ -0,0 +1,13 @@
+package in.temple.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempleBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/in/temple/backend/TempleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package in.temple.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
